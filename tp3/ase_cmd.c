d4fe18594ddfec352e21e18a8c1ab23593818789
Lheureux Axel <lheureux@a10p20.fil.univ-lille1.fr>
2016-04-06 17:11:40 +0200

    good start

--- tp3/ase_cmd.c
+++ tp3/ase_cmd.c
@@ -8,45 +8,44 @@
 #include <linux/pid.h>
 #include <asm/uaccess.h>
 
-#define ASE_CMD_BUFFER_LEN	6
-static char		ase_cmd_buffer[ASE_CMD_BUFFER_LEN];
+#define ASE_CMD_BUFFER_LEN 6
+static char	ase_cmd_buffer[ASE_CMD_BUFFER_LEN];
 static struct pid	*ase_cmd_pid;
+
+static int
+ase_cmd_proc_show(struct seq_file *m, void *v)
+{
+  seq_printf(m, "Je suis une loutre");
+  return 0;
+}
 
 static int
 ase_cmd_proc_open(struct inode *inode, struct file *file)
 {
-  printk(KERN_INFO "ASE_CMD: Open has been called");
-  return 0;
-}
-
-static void
-ase_cmd_new_pid(long res)
-{
-  int	pid;
-
-  pid = res;
-  rcu_read_lock();
-  ase_cmd_pid = find_vpid(pid);
-  rcu_read_unlock();
+  return single_open(file, ase_cmd_proc_show, NULL);
 }
 
 static ssize_t
 ase_cmd_proc_write(struct file *filp, const char __user *buff,
 		   size_t len, loff_t *data)
 {
-  long	res;
-  
-  printk(KERN_INFO "ASE_CMD: Wirte has been called");
+  long res;
+  printk(KERN_INFO "ASE_CMD: Write has been called");
   if (len > (ASE_CMD_BUFFER_LEN - 1)) {
     printk(KERN_INFO "ASE_CMD: error, input too long");
     return -EINVAL;
   }
-  else if (copy_from_user(ase_cmd_buffer, buff, len))
+  else if (copy_from_user(ase_cmd_buffer, buff, len)) {
     return -2;
+  }
   ase_cmd_buffer[len] = 0;
 
   kstrtol(ase_cmd_buffer, 0, &res);
-  ase_cmd_new_pid(res);
+
+  rcu_read_lock();
+  find_vpid((int)res);
+  rcu_read_unlock();
+
   return len;
 }
 
@@ -74,4 +73,5 @@ ase_cmd_proc_exit(void)
 
 module_init(ase_cmd_proc_init);
 module_exit(ase_cmd_proc_exit);
+
 MODULE_LICENSE("GPL");
