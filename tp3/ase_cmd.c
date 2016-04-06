68e253053cc46504295fdc046b2f28e89a60d778
Lheureux Axel <lheureux@a10p20.fil.univ-lille1.fr>
2016-04-06 15:33:55 +0200

    new reasearch and implementation started(might not be working though)

--- tp3/ase_cmd.c
+++ tp3/ase_cmd.c
@@ -5,7 +5,12 @@
 #include <linux/seq_file.h>
 #include <linux/jiffies.h>
 #include <linux/string.h>
+#include <linux/pid.h>
 #include <asm/uaccess.h>
+
+#define ASE_CMD_BUFFER_LEN	6
+static char		ase_cmd_buffer[ASE_CMD_BUFFER_LEN];
+static struct pid	*ase_cmd_pid;
 
 static int
 ase_cmd_proc_open(struct inode *inode, struct file *file)
@@ -14,11 +19,34 @@ ase_cmd_proc_open(struct inode *inode, struct file *file)
   return 0;
 }
 
+static void
+ase_cmd_new_pid(long res)
+{
+  int	pid;
+
+  pid = res;
+  rcu_read_lock();
+  ase_cmd_pid = find_vpid(pid);
+  rcu_read_unlock();
+}
+
 static ssize_t
 ase_cmd_proc_write(struct file *filp, const char __user *buff,
 		   size_t len, loff_t *data)
 {
+  long	res;
+  
   printk(KERN_INFO "ASE_CMD: Wirte has been called");
+  if (len > (ASE_CMD_BUFFER_LEN - 1)) {
+    printk(KERN_INFO "ASE_CMD: error, input too long");
+    return -EINVAL;
+  }
+  else if (copy_from_user(ase_cmd_buffer, buff, len))
+    return -2;
+  ase_cmd_buffer[len] = 0;
+
+  kstrtol(ase_cmd_buffer, 0, &res);
+  ase_cmd_new_pid(res);
   return len;
 }
 
